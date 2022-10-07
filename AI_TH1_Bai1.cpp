f559fcec3772c8c2e246ff458bb6a71d64c9a587
binhhong1412 <binhhong1412@gmail.com>
2022-10-07 10:11:33 +0700

    final commit 1

--- AI_TH1_Bai1.cpp
+++ AI_TH1_Bai1.cpp
@@ -4,15 +4,16 @@
 #include <queue>
 using namespace std;
 
-typedef pair<int, int> jugs;                // define structure data for jugs
-void BFS(int a, int b, int target);         // define BFS function
-void printpath(map<jugs, jugs> mp, jugs u); // define function print results
+typedef pair<int, int> jugs;                              // define structure data for jugs
+void BFS(int a, int b, int target);                       // define BFS(Breadth-First Search) function
+void printresult(map<jugs, jugs> node, jugs currentNode); // define function print results
 void Input(int &, int &, int &);
 
 int main()
 {
     int a, b, target;
     /// viet ham nhap a,b, target tu ban phim
+    // CODE HERE
     Input(a, b, target);
     BFS(a, b, target);
     return 0;
@@ -20,92 +21,129 @@ int main()
 
 void Input(int &a, int &b, int &target)
 {
-    cout << "Nhap a: ";
+    cout << "Nhap gioi han Jug A: ";
     cin >> a;
-    cout << "Nhap b: ";
+    cout << "Nhap gioi han Jug B: ";
     cin >> b;
-    cout << "Nhap target: ";
+    cout << "Nhap target of da Jug: ";
     cin >> target;
 }
 
-void printpath(map<jugs, jugs> mp, jugs u)
+void printresult(map<jugs, jugs> node, jugs currentNode)
 {
-    if (u.first == 0 && u.second == 0)
+    if (currentNode.first == 0 && currentNode.second == 0)
     {
         cout << 0 << " " << 0 << endl;
         return;
     }
-    printpath(mp, mp[u]);
-    cout << u.first << " " << u.second << endl;
+    printresult(node, node[currentNode]);
+    cout << currentNode.first << " " << currentNode.second << endl;
 }
 
 void BFS(int a, int b, int target)
 {
-    map<jugs, int> m;        // kiem tra cac node da duyet: 1 da duyet, 0 chua duyet (default = 0)
-    bool isSolvable = false; // ton tai ket qua
-    map<jugs, jugs> mp;      // Graph
-    queue<jugs> q;           // Queue de duyet cay bang BFS
+    map<jugs, int> checkedList; // kiem tra cac node da duyet: 1 da duyet, 0 chua duyet (default = 0)
+    bool isSolvable = false;    // ton tai ket qua
+    map<jugs, jugs> node;       // Graph
+    queue<jugs> q;              // Queue de duyet cay bang BFS
 
     q.push(make_pair(0, 0)); // Bat dau duyet cay cho ket qua vao hanh doi
+    // Queue: 0,0 3,0 4,0
     while (!q.empty())
     {
-        jugs u = q.front(); // lay phan tu dau tien
-        q.pop();            // xoa phan tu dau tien khoi hang doi
-        if (m[u] == 1)      // node da duyet thi pass
+        jugs currentNode = q.front();      // lay phan tu dau tien
+        q.pop();                           // xoa phan tu dau tien khoi hang doi
+        if (checkedList[currentNode] == 1) // node da duyet thi pass
             continue;
-        m[{u.first, u.second}] = 1; // da duyet
+        checkedList[{currentNode.first, currentNode.second}] = 1; // da duyet
 
-        if (u.first == target || u.second == target)
+        if (currentNode.first == target || currentNode.second == target)
         {
             isSolvable = true;
-
-            printpath(mp, u);
             // CODE HERE
+            if (currentNode.first == target && currentNode.second != 0)
+            {
+                node[{currentNode.first, 0}] = currentNode;
+                currentNode.second = 0;
+            }
+            if (currentNode.first == target && currentNode.second != 0)
+            {
+                node[{0, currentNode.second}] = currentNode;
+                currentNode.first = 0;
+            }
+            printresult(node, currentNode);
             return;
         }
         // ======= Operation =====
         // ## Fill
         // fill into jug 1
-        if (m[{a, u.second}] != 1)
+        if (checkedList[{a, currentNode.second}] != 1)
         {
-            q.push({a, u.second});
-            mp[{a, u.second}] = u;
+            q.push({a, currentNode.second});
+            node[{a, currentNode.second}] = currentNode; // Set father
         }
         // fill into jug 2
         // CODE HERE
-        // ## Transfer of water
-        int d = b - u.second; // from jug 1 to jug 2
-        if (u.first >= d)
+
+        if (checkedList[{currentNode.first, b}] != 1)
         {
-            int c = u.first - d; // transfer a little
-            if (m[{c, b}] != 1)
+            q.push({currentNode.first, b});
+            node[{currentNode.first, b}] = currentNode;
+        }
+        // ## Transfer of water
+        // from jug 1 to jug 2
+        int unfilledSpace1 = b - currentNode.second;
+        if (currentNode.first >= unfilledSpace1)
+        {
+            int remainingValue1 = currentNode.first - unfilledSpace1; // transfer a little
+            if (checkedList[{remainingValue1, b}] != 1)
             {
-                q.push({c, b});
-                mp[{c, b}] = u;
+                q.push({remainingValue1, b});
+                node[{remainingValue1, b}] = currentNode;
             }
         }
         else
         {
-            int c = u.first + u.second;
-            if (m[{0, c}] != 1)
+            int remainingValue1 = currentNode.first + currentNode.second;
+            if (checkedList[{0, remainingValue1}] != 1)
             { /// transfer all
-                q.push({0, c});
-                mp[{0, c}] = u;
+                q.push({0, remainingValue1});
+                node[{0, remainingValue1}] = currentNode;
             }
         }
         // from jug 2 to j1
         // CODE HERE
 
-        if (m[{u.first, 0}] != 1)
+        int unfilledSpace2 = a - currentNode.first;
+        if (currentNode.second >= unfilledSpace2)
         {
-            q.push({u.first, 0});
-            mp[{u.first, 0}] = u;
+            int remainingValue2 = currentNode.second - unfilledSpace2; // transfer a little
+            if (checkedList[{a, remainingValue2}] != 1)
+            {
+                q.push({a, remainingValue2});
+                node[{a, remainingValue2}] = currentNode;
+            }
+        }
+        else
+        {
+            int remainingValue2 = currentNode.first + currentNode.second;
+            if (checkedList[{remainingValue2, 0}] != 1)
+            { /// transfer all
+                q.push({remainingValue2, 0});
+                node[{remainingValue2, 0}] = currentNode;
+            }
         }
 
-        if (m[{0, u.second}] != 1)
+        if (checkedList[{currentNode.first, 0}] != 1)
         {
-            q.push({0, u.second});
-            mp[{0, u.second}] = u;
+            q.push({currentNode.first, 0});
+            node[{currentNode.first, 0}] = currentNode;
+        }
+
+        if (checkedList[{0, currentNode.second}] != 1)
+        {
+            q.push({0, currentNode.second});
+            node[{0, currentNode.second}] = currentNode;
         }
     }
     if (!isSolvable)
